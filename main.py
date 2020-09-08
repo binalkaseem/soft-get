8c67033d7d1abca828c7cd0e8b54a7f198aa0983
binalkaseem <54190922+binalkaseem@users.noreply.github.com>
2020-09-08 06:44:21 +0100

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,11 @@
+def on_forever():
+    pass
+basic.forever(on_forever)
+1 * 1
+def on_button_pressed_a():
+    pass
+input.on_button_pressed(Button.A, on_button_pressed_a)
+basic.show_number(0)
+basic.show_string("Hello!")
+radio.send_number(0)
+hello world
---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.forever(function on_forever() {
+    
+})
+1 * 1
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+})
+basic.showNumber(0)
+basic.showString("Hello!")
+radio.sendNumber(0)
